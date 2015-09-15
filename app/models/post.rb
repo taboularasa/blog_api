45db81cc2e52c9c68a482f18afdd936245fcf12b
David Elliott <david@gophilosophie.com>
2015-09-14 22:53:11 -0700

    adds more secure validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true, uniqueness: true
+  validate :blue_free_title
+  validate :red_free_title
+
+  private
+
+  def blue_free_title
+    return unless title && title.downcase.include?('blue')
+    errors.add(:title, 'can not have blue')
+  end
+
+  def red_free_title
+    return unless title && title.downcase.include?('red')
+    errors.add(:title, 'can not have red')
+  end
 end
